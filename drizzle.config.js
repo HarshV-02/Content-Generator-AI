1173e12caccd8e61ea882193ecae7db0f1974e08
HarshV-02 <dabbup111@gmail.com>
2025-01-11 14:20:48 +0530

    Initial Commit

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.tsx",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: 'postgresql://AI-Content-Generator_owner:eLSs5dCtUF8g@ep-empty-dream-a16uzh06.ap-southeast-1.aws.neon.tech/AI-Content-Generator?sslmode=require',
+
+    }
+  };
+  
